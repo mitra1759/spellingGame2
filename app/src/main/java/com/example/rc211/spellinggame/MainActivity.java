dc7ea0d3f16e387579f6007791ace9b2e0090f24
mitra1759 <42783913+mitra1759@users.noreply.github.com>
2018-10-16 14:08:58 -0500

    features

--- app/src/main/java/com/example/rc211/spellinggame/MainActivity.java
+++ app/src/main/java/com/example/rc211/spellinggame/MainActivity.java
@@ -1,5 +1,11 @@
 package com.example.rc211.spellinggame;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Locale;
 
 import android.app.Activity;
@@ -9,6 +15,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements
         TextToSpeech.OnInitListener {
@@ -17,12 +25,14 @@ public class MainActivity extends Activity implements
     private TextToSpeech tts;
     EditText wordInput;
     Button enterButton;
+    InputStream inputStream;//creates a fileinputstream to load data from the file
+    ArrayList dictionary =new ArrayList();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//        load();
         tts = new TextToSpeech(this, this);
         wordInput = findViewById(R.id.wordInput);
         enterButton = findViewById(R.id.enterButton);
@@ -31,6 +41,8 @@ public class MainActivity extends Activity implements
 
             @Override
             public void onClick(View arg0) {
+//                Toast.makeText(getApplicationContext(),dictionary.get((int)(Math.random()*dictionary.size()))+"",Toast.LENGTH_SHORT).show();
+
                 speakOut();
             }
 
@@ -74,5 +86,25 @@ public class MainActivity extends Activity implements
 
         tts.speak(text, TextToSpeech.QUEUE_FLUSH, null,"id1");
     }
+//    public void load() {//loads the stats
+//        try {
+//            inputStream = this.getResources().openRawResource(R.raw.dictionary);
+//            InputStreamReader inputReader = new InputStreamReader(inputStream);//converts the byte stream to characters
+//            BufferedReader bufferedReader = new BufferedReader(inputReader);//acts as a buffer for the info; processes it?
+//
+//            try {
+//                String line = bufferedReader.readLine();//saves the line read to a temp variable to avoid reading next line during check
+//                while (line != null) {
+//                    dictionary.add(line);
+//                }
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
 }
 
